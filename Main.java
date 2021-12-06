75f181fe11d97def14d59a71c13820cc8ee8c202
AnnieLeonhard <82411471+AnnieLeonhard@users.noreply.github.com>
2021-12-05 20:31:32 -0500

    Added questions for the project
    Added questions for the project

--- Main.java
+++ Main.java
@@ -19,6 +19,11 @@ public class Main {
 
     }
 
+    /**
+     * A department has a dean, who should be a teacher of that department,
+     * otherwise an exception must be
+     * thrown.
+     */
     public static void question1() {
         /*
          * 1) Teacher speciality = Department if not exception thrown
@@ -39,8 +44,68 @@ public class Main {
         // throw new ArithmeticException("The teacher is not from this department!");
         // }
     }
-                        
+
+    /**
+     * Teachers, students, and staffs are added/assigned to a department, first by
+     * loading/reading from a text
+     * file during the first execution of the application. Moreover, new teachers,
+     * students, and staff may be
+     * added/assigned using the graphical user interface.
+     */
     public static void question2() {
+        // idk - check solution in stackoverflow(Scanner)
+    }
+
+    /**
+     * - Trying to add a teacher, a student, or a staff member to an inexistent
+     * department (based on department
+     * id) should rise/throw an exception.
+     */
+    public static void question3() {
+        // how we should check that in real time?
+    }
+
+    /**
+     * - Trying to add a teacher, a student, or a staff member that already exists/
+     * added (based on the id) to
+     * department should throw an exception.
+     */
+    public static void question4() {
+
+    }
+
+    /**
+     * - Each new teacher, student, and staff added to a department should be stored
+     * to the text file before
+     * exiting the application, generating a new version of the text file. So, if
+     * the text file does exist (which is the
+     * case), new teachers, students, or staffs should be appended. There are files
+     * of teachers, students, and
+     * staffs.
+     */
+    public static void question5() {
+
+    }
+
+    /**
+     * - The customized exceptions handling for the cases above must be implemented.
+     */
+    public static void question6() {
+
+    }
+
+    /**
+     * - Test sets should be generated for testing the effectiveness of each method.
+     */
+    public static void question7() {
+
+    }
+
+    /**
+     * - Samples of teachers, staffs, and students are provided along with the
+     * present document.
+     */
+    public static void question8() {
 
     }
 }
